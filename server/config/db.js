d72c76bb291245590c1168e76fa9321aa58010d8
IbrahimOwner2020 <kidibra2020@gmail.com>
2022-07-11 10:21:48 +0300

    fix: error-handling-mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI);
-	console.log(
-		`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
-	);
+	await mongoose
+		.connect(process.env.MONGO_URI)
+		.then((conn) => {
+			console.log(
+				`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+			);
+		})
+		.catch((error) => {
+			console.log(
+				`Failed to connect to mongodb server due to: ${error.message}`
+					.red.bold
+			);
+			console.log(error);
+		});
 };
 
 module.exports = connectDB;
